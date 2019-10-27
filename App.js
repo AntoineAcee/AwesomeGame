fdcc820509698a6dcfb82703f0b71a0ccac38b86
AntoineAcee <antoine.lefevre@supinternet.fr>
2019-10-27 08:16:46 +0100

    Add Player State and Movements

---
+++ App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { 
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Animated,
+  Image,
+  ImageBackground
+} from 'react-native';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movePlayerValue: new Animated.Value(60),
+      playerSide: 'left',
+      points: 0,
+    }
+  }
+
+  render() {
+    return (
+      <ImageBackground source={require('./app/img/bg.jpg')} style={styles.container}>
+
+        <View style={{ flex: 1, alignItems: 'center', marginTop: 80 }}>
+          <View style={styles.points}>
+            <Text style={{ fontWeight: 'bold', fontSize: 40, color: '#666' }}> {this.state.points} </Text>
+          </View>
+        </View>
+
+        <Animated.Image source={require('./app/img/main_car.png')}
+        style={{
+          height: 100,
+          width: 100,
+          position: 'absolute',
+          zIndex: 1,
+          bottom: 0,
+          resizeMode: 'stretch',
+          transform: [
+            { translateX: this.state.movePlayerValue }
+          ]
+        }}></Animated.Image>
+
+        <View style={styles.controls}>
+          <Text style={styles.left} onPress={ () => this.movePlayerValue('left') }> {'<'} </Text>
+          <Text style={styles.right} onPress={ () => this.movePlayerValue('right') }> {'>'} </Text>
+        </View>
+
+      </ImageBackground>
+    );
+  }
+
+  movePlayerValue(direction) {
+    // Move to right
+    if (direction == 'right') {
+      this.setState({ playerSide: 'right' })
+    
+      Animated.spring(
+        this.state.movePlayerValue,
+        {
+          toValue: Dimensions.get('window').width - 160,
+          tension: 120,
+        }
+      ).start();
+    } else if (direction == 'left') {
+      this.setState({ playerSide: 'left' })
+    
+      Animated.spring(
+        this.state.movePlayerValue,
+        {
+          toValue: 60,
+          tension: 120,
+        }
+      ).start();
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+    resizeMode: 'cover',
+  },
+  points: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#FFF',
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  controls: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    bottom: 100,
+  },
+  right: {
+    flex: 1,
+    color: '#FFF',
+    fontSize: 60,
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+  left: {
+    flex: 1,
+    color: '#FFF',
+    fontSize: 60,
+    fontWeight: 'bold',
+    textAlign: 'right'
+  }
+});
